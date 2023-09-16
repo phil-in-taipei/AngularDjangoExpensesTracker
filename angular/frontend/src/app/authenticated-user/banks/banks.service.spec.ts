6006aefb74e22bcd66f23f4243d4736827236816
phil-in-taipei <sweeney.phil@gmail.com>
2023-09-16 16:28:30 +0800

    Angular unit tests for bank service

--- angular/frontend/src/app/authenticated-user/banks/banks.service.spec.ts
+++ angular/frontend/src/app/authenticated-user/banks/banks.service.spec.ts
@@ -1,16 +1,73 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, fakeAsync, flush  } from '@angular/core/testing';
+import { HttpTestingController, HttpClientTestingModule
+} from '@angular/common/http/testing';
+
+import { authData } from '../../test-data/authentication-tests/authentication-data';
+import { AuthService } from '../../authentication/auth.service';
 
 import { BanksService } from './banks.service';
+import { environment } from 'src/environments/environment';
 
-describe('BanksService', () => {
+import { 
+  banksData 
+} from 'src/app/test-data/authenticated-user-module-tests/banks-tests/banks-data';
+
+fdescribe('BanksService', () => {
   let service: BanksService;
+  let httpTestingController: HttpTestingController;
+  let authServiceSpy: jasmine.SpyObj<AuthService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    authServiceSpy = jasmine.createSpyObj('AuthService', ['getAuthToken']);
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+      providers: [
+        BanksService,
+        { provide: AuthService, useValue: authServiceSpy }
+      ]
+    });
     service = TestBed.inject(BanksService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    authServiceSpy = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should handle error due to network error when fetching banks', () => {
+    authServiceSpy.getAuthToken.and.returnValue(authData.token);
+    service.fetchAllBanks().subscribe(
+      () => {},
+      error => {
+        expect(error).toBeTruthy();
+      }
+    );
+  
+    const request = httpTestingController.expectOne({
+      method: 'GET',
+      url:`${environment.apiUrl}/api/financial-accounts/banks/`,
+    });
+
+    request.error(new ErrorEvent('network error'));
+    
+  });
+
+  it('should return banks array from the api', 
+    fakeAsync(() => {
+    authServiceSpy.getAuthToken.and.returnValue(authData.token);
+    service.fetchAllBanks().subscribe(response => {
+      expect(response).toEqual(banksData);
+    });
+
+    const request = httpTestingController.expectOne({
+      method: 'GET',
+      url:`${environment.apiUrl}/api/financial-accounts/banks/`,
+    });
+
+    request.flush(banksData);
+
+    flush();
+
+  }));
 });
---
+++ angular/frontend/src/app/test-data/authenticated-user-module-tests/banks-tests/banks-data.ts
@@ -0,0 +1,12 @@
+import { BankModel } from "src/app/models/bank.model";
+
+export const banksData: BankModel[] = [
+    {
+        id: 1,
+        bank_name: 'Test Bank 1',
+    },
+    {
+        id: 2,
+        bank_name: 'Test Bank 2',
+    }
+];
