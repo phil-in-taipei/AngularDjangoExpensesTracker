14929ead4ee1e76d08d5bc48bbc017f0ec2b0919
phil-in-taipei <sweeney.phil@gmail.com>
2023-09-22 16:38:01 +0800

    Savings account service unit test for fetching savings accounts

--- angular/frontend/src/app/authenticated-user/finance/savings-accounts/savings-accounts.service.spec.ts
+++ angular/frontend/src/app/authenticated-user/finance/savings-accounts/savings-accounts.service.spec.ts
@@ -1,16 +1,74 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, fakeAsync, flush  } from '@angular/core/testing';
+import { HttpTestingController, HttpClientTestingModule
+} from '@angular/common/http/testing';
+
+import { authData } from '../../../test-data/authentication-tests/authentication-data';
+import { AuthService } from '../../../authentication/auth.service';
+import { environment } from 'src/environments/environment';
 
 import { SavingsAccountsService } from './savings-accounts.service';
 
-describe('SavingsAccountsService', () => {
+import { 
+  savingsAccountsData
+} from 'src/app/test-data/authenticated-user-module-tests/finance-module-tests/savings-accounts-tests/savings-accounts-data';
+
+fdescribe('SavingsAccountsService', () => {
   let service: SavingsAccountsService;
+  let httpTestingController: HttpTestingController;
+  let authServiceSpy: jasmine.SpyObj<AuthService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    authServiceSpy = jasmine.createSpyObj('AuthService', ['getAuthToken']);
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+      providers: [
+        SavingsAccountsService,
+        { provide: AuthService, useValue: authServiceSpy }
+      ]
+    });
     service = TestBed.inject(SavingsAccountsService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    authServiceSpy = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should handle error due to network error when ' 
+    + 'fetching savings accounts', () => {
+      authServiceSpy.getAuthToken.and.returnValue(authData.token);
+      service.fetchAllSavingsAccounts().subscribe(
+        () => {},
+        error => {
+          expect(error).toBeTruthy();
+        }
+      );
+    
+      const request = httpTestingController.expectOne({
+        method: 'GET',
+        url:`${environment.apiUrl}/api/financial-accounts/savings-accounts/`,
+      });
+
+      request.error(new ErrorEvent('network error'));
+      
+  });
+
+  it('should return savings accounts array from the api', 
+    fakeAsync(() => {
+      authServiceSpy.getAuthToken.and.returnValue(authData.token);
+      service.fetchAllSavingsAccounts().subscribe(response => {
+        expect(response).toEqual(savingsAccountsData);
+      });
+
+      const request = httpTestingController.expectOne({
+        method: 'GET',
+        url:`${environment.apiUrl}/api/financial-accounts/savings-accounts/`,
+      });
+
+      request.flush(savingsAccountsData);
+
+      flush();
+
+  }));
 });
