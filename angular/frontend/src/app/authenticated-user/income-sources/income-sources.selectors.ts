a99a88b34d6e1c569095798d271875db6fbd5880
mikalikahi <sweeney.phil@gmail.com>
2023-10-13 16:18:18 +0800

    Income sources ngrx selectors -- Angular

---
+++ angular/frontend/src/app/authenticated-user/income-sources/income-sources.selectors.ts
@@ -0,0 +1,34 @@
+import {createFeatureSelector, createSelector} from '@ngrx/store';
+
+import { IncomeSourcesState } from './income-sources.reducers';
+import * as fromIncomeSources from './income-sources.reducers';
+
+export const selectIncomeSourcesState = 
+            createFeatureSelector<IncomeSourcesState>("income");
+
+
+export const selectIncomeSourcesById = (id:number) => createSelector(
+        selectIncomeSourcesState,
+        incomeSourcesState => incomeSourcesState.entities[id]
+    );
+
+export const selectAllIncomeSources = createSelector(
+        selectIncomeSourcesState,
+        fromIncomeSources.selectAll
+    );
+        
+export const incomeSourcesLoaded = createSelector(
+        selectIncomeSourcesState,
+        incomeSourcesState => incomeSourcesState.incomeSourcesLoaded
+    );
+
+export const savingsAccountSubmissionErrorMsg = createSelector(
+        selectIncomeSourcesState,
+        incomeSourcesState => incomeSourcesState.errorMessage
+    );
+      
+export const savingsAccountSubmissionSuccessMsg = createSelector(
+        selectIncomeSourcesState,
+        incomeSourcesState => incomeSourcesState.successMessage
+    );
+              
