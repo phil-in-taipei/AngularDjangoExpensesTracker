f6b9dd021d1149627cd1401e795efb82b2316e30
phil-in-taipei <sweeneyphilip11@gmail.com>
2023-11-08 15:28:00 +0800

    Transaction service methods to submit, fetch by month, and delete deposits and withdrawals with corresponding typescript models

--- angular/frontend/src/app/authenticated-user/finance/transactions/transactions.service.ts
+++ angular/frontend/src/app/authenticated-user/finance/transactions/transactions.service.ts
@@ -1,9 +1,88 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { environment } from '../../../../environments/environment';
+import { AuthService } from '../../../authentication/auth.service';
+
+import { 
+  DepositCreateModel, 
+  TransactionDeletionResponse, 
+  TransactionModel, WithdrawalCreateModel 
+} from 'src/app/models/transaction-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransactionsService {
 
-  constructor() { }
+  constructor(private http: HttpClient,
+    private authService: AuthService) { }
+
+  deleteDeposit(id: number) {
+    let token = this.authService.getAuthToken();
+    return this.http.delete<TransactionDeletionResponse>(
+      `${environment.apiUrl}/api/transactions/submit/deposit/${id}/`,
+        {
+          headers: new HttpHeaders({ 'Authorization': `Token ${token}` })
+        })
+  }
+
+  deleteWithdrawal(id: number) {
+    let token = this.authService.getAuthToken();
+    return this.http.delete<TransactionDeletionResponse>(
+      `${environment.apiUrl}/api/transactions/submit/withdrawal/${id}/`,
+        {
+          headers: new HttpHeaders({ 'Authorization': `Token ${token}` })
+        })
+  }
+
+  fetchAccountTransactionsByMonthAndYear(month: number, year: number, accountId: number) {
+    let token = this.authService.getAuthToken();
+    return this.http.get<TransactionModel[]>(
+      `${environment.apiUrl}/api/transactions/account-transactions/by-month-year/${month}/${year}/${accountId}/`,
+        {
+          headers: new HttpHeaders({ 'Authorization': `Token ${token}` })
+        })
+  }
+  
+
+  fetchDepositsByMonthAndYear(month: number, year: number) {
+    let token = this.authService.getAuthToken();
+    return this.http.get<TransactionModel[]>(
+      `${environment.apiUrl}/api/transactions/deposits/by-month-year/${month}/${year}/`,
+        {
+            headers: new HttpHeaders({ 'Authorization': `Token ${token}` })
+         })
+  }
+
+  fetchWithdrawalsByMonthAndYear(month: number, year: number) {
+    let token = this.authService.getAuthToken();
+    return this.http.get<TransactionModel[]>(
+      `${environment.apiUrl}/api/transactions/withdrawals/by-month-year/${month}/${year}/`,
+        {
+            headers: new HttpHeaders({ 'Authorization': `Token ${token}` })
+         })
+  }
+
+
+  submitNewDeposit(
+    submissionForm:DepositCreateModel) {
+    let token = this.authService.getAuthToken();
+    return this.http.post<TransactionModel>(
+      `${environment.apiUrl}/api/transactions/submit/deposit/`, submissionForm,
+      {
+        headers: new HttpHeaders({ 'Authorization': `Token ${token}` })
+      });
+  }
+
+  submitNewWithdrawal(
+    submissionForm:WithdrawalCreateModel) {
+    let token = this.authService.getAuthToken();
+    return this.http.post<TransactionModel>(
+      `${environment.apiUrl}/api/transactions/submit/withdrawal/`, submissionForm,
+      {
+        headers: new HttpHeaders({ 'Authorization': `Token ${token}` })
+      });
+  }
+
 }
---
+++ angular/frontend/src/app/models/transaction-model.ts
@@ -0,0 +1,26 @@
+export interface DepositCreateModel {
+    amount: number;
+    date: string;
+    income_source: number;
+    savings_account: number;
+ }
+
+export interface TransactionDeletionResponse {
+   id: number;
+   message: string;
+}
+
+export interface TransactionModel {
+    id: number;
+    transaction: string;
+    amount: number;
+    date: string;
+    income_source?: number;
+    savings_account: number;
+}
+
+export interface WithdrawalCreateModel {
+    amount: number;
+    date: string;
+    savings_account: number;
+ }
