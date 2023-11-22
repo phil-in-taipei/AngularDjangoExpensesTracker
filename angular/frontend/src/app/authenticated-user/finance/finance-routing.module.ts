50321f590957379382233a4c8cb035e8b99c90a2
phil-in-taipei <sweeneyphilip11@gmail.com>
2023-11-22 10:24:36 +0800

    Finished child template/component implementation of withdrawal post success message and subsequent updating of savings account balance in ngrx state

--- angular/frontend/src/app/authenticated-user/finance/finance-routing.module.ts
+++ angular/frontend/src/app/authenticated-user/finance/finance-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FinanceComponent } from './finance.component';
-import { CreateAccountComponent } from './savings-accounts/create-account/create-account.component';
-import { CreateDepositComponent } from './transactions/deposits/create-deposit/create-deposit.component';
-import { DepositsListComponent } from './transactions/deposits/deposits-list/deposits-list.component';
-import { EditAccountComponent } from './savings-accounts/edit-account/edit-account.component';
-import { SavingsAccountsComponent } from './savings-accounts/savings-accounts-list/savings-accounts.component';
+import { 
+  CreateAccountComponent 
+} from './savings-accounts/create-account/create-account.component';
+import { 
+  CreateDepositComponent 
+} from './transactions/deposits/create-deposit/create-deposit.component';
+import { 
+  CreateWithdrawalComponent 
+} from './transactions/withdrawals/create-withdrawal/create-withdrawal.component';
+import { 
+  DepositsListComponent 
+} from './transactions/deposits/deposits-list/deposits-list.component';
+import { 
+  EditAccountComponent 
+} from './savings-accounts/edit-account/edit-account.component';
+import { 
+  SavingsAccountsComponent 
+} from './savings-accounts/savings-accounts-list/savings-accounts.component';
 
 const routes: Routes = [
   { path: '', component: FinanceComponent, children: [ 
@@ -13,6 +26,7 @@ const routes: Routes = [
       { path: 'edit-account/:id', component: EditAccountComponent },
       { path: 'create-account', component: CreateAccountComponent },
       { path: 'create-deposit', component: CreateDepositComponent },
+      { path: 'create-withdrawal', component: CreateWithdrawalComponent },
       { path: 'monthly-deposits', component: DepositsListComponent },
     ] 
   },
--- angular/frontend/src/app/authenticated-user/finance/transactions/withdrawals/create-withdrawal/withdrawal-submit-form/newly-created-withdrawal/create-withdrawal-new-balance/create-withdrawal-new-balance.component.ts
+++ angular/frontend/src/app/authenticated-user/finance/transactions/withdrawals/create-withdrawal/withdrawal-submit-form/newly-created-withdrawal/create-withdrawal-new-balance/create-withdrawal-new-balance.component.ts
@@ -1,4 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable, of } from "rxjs";
+
+import { AppState } from 'src/app/reducers';
+import { SavingsAccountModel } from 'src/app/models/savings-account.model';
+import { 
+  SavingsAccountWithdrawalSaved 
+} from 'src/app/authenticated-user/finance/savings-accounts/savings-accounts.actions';
+import { TransactionModel } from 'src/app/models/transaction-model';
+import { 
+  SavingsAccountMessagesCleared 
+} from 'src/app/authenticated-user/finance/savings-accounts/savings-accounts.actions';
+import { 
+  savingsAccountSuccessMsg 
+} from 'src/app/authenticated-user/finance/savings-accounts/savings-accounts.selectors';
 
 @Component({
   selector: 'app-create-withdrawal-new-balance',
@@ -7,9 +22,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateWithdrawalNewBalanceComponent implements OnInit {
 
-  constructor() { }
+  @Input() withdrawal: TransactionModel;
+  @Input() savingsAccount: SavingsAccountModel;
+  accountSubmitSuccessMsg$: Observable<string | undefined> = of(undefined);
+
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
+    let payload = {
+      amount: this.withdrawal.amount,
+      savingsAccount: this.savingsAccount
+    }
+
+    this.store.dispatch(
+      new SavingsAccountWithdrawalSaved(payload)
+    );
+    this.accountSubmitSuccessMsg$ = this.store.pipe(
+      select(savingsAccountSuccessMsg)
+    );
   }
 
+  onClearStatusMsgs() {
+    this.store.dispatch(new SavingsAccountMessagesCleared());
+  }
 }
