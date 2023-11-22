d46cab2cd9883378d5e268a4564bace54c944d4d
phil-in-taipei <sweeneyphilip11@gmail.com>
2023-11-22 15:05:51 +0800

    Withdrawal related methods in TransactionsService unit test and mock data

--- angular/frontend/src/app/authenticated-user/finance/transactions/transactions.service.spec.ts
+++ angular/frontend/src/app/authenticated-user/finance/transactions/transactions.service.spec.ts
@@ -13,6 +13,10 @@ import {
   createdDeposit, depositsData, depositDeletionResponse, newDepositData
 } from 'src/app/test-data/authenticated-user-module-tests/finance-module-tests/savings-accounts-tests/deposits-data';
 
+import { 
+  createdWithdrawal, newWithdrawalData, withdrawalDeletionResponse, withdrawalsData 
+} from 'src/app/test-data/authenticated-user-module-tests/finance-module-tests/savings-accounts-tests/withdrawals-data';
+
 fdescribe('TransactionsService', () => {
   let httpTestingController: HttpTestingController;
   let authServiceSpy: jasmine.SpyObj<AuthService>;
@@ -56,19 +60,44 @@ fdescribe('TransactionsService', () => {
       
   });
 
+  it("should handle network error when fetching users' " 
+    + "withdrawals for this month", () => {
+      const today: Date = new Date();
+      authServiceSpy.getAuthToken.and.returnValue(authData.token);
+      service.fetchWithdrawalsByMonthAndYear(
+        today.getMonth() + 1, today.getFullYear()).subscribe(
+        () => {},
+        error => {
+          expect(error).toBeTruthy();
+        }
+      );
+      
+      // getMonth() returns values beginning with '0' -- 1 must be added to get
+      // current month value to be correctly queried on backend 
+      const request = httpTestingController.expectOne({
+        method: 'GET',
+        url:`${environment.apiUrl}/api/transactions/withdrawals/by-month-year/` 
+        + `${today.getMonth() + 1}/${today.getFullYear()}/`,
+      });
+
+      request.error(new ErrorEvent('network error'));
+      
+  });
+
+
 
   it("should return the array of users' deposits for this month from the api", 
     fakeAsync(() => {
     const today: Date = new Date();
-    authServiceSpy.getAuthToken.and.returnValue(authData.token);
 
-    console.log(depositsData);
+    authServiceSpy.getAuthToken.and.returnValue(authData.token);
 
     service.fetchDepositsByMonthAndYear(
       today.getMonth() + 1, today.getFullYear()
       ).subscribe(response => {
       expect(response).toEqual(depositsData);
     });
+
     const request = httpTestingController.expectOne({
       method: 'GET',
       url:`${environment.apiUrl}/api/transactions/deposits/by-month-year/` 
@@ -76,6 +105,31 @@ fdescribe('TransactionsService', () => {
     });
 
     request.flush(depositsData);
+
+    flush();
+
+  }));
+
+
+  it("should return the array of users' withdrawals for this month from the api", 
+    fakeAsync(() => {
+    const today: Date = new Date();
+
+    authServiceSpy.getAuthToken.and.returnValue(authData.token);
+
+    service.fetchWithdrawalsByMonthAndYear(
+      today.getMonth() + 1, today.getFullYear()
+      ).subscribe(response => {
+      expect(response).toEqual(withdrawalsData);
+    });
+
+    const request = httpTestingController.expectOne({
+      method: 'GET',
+      url:`${environment.apiUrl}/api/transactions/withdrawals/by-month-year/` 
+          + `${today.getMonth() + 1}/${today.getFullYear()}/`,
+    });
+
+    request.flush(withdrawalsData);
 
     flush();
 
@@ -101,9 +155,30 @@ fdescribe('TransactionsService', () => {
 
   }));
 
+
+  it('should return a new withdrawal object from backend after submitting ' 
+    + 'data to create a new object', 
+    fakeAsync(() => {
+      authServiceSpy.getAuthToken.and.returnValue(authData.token);
+
+      service.submitNewWithdrawal(newWithdrawalData).subscribe(response => {
+        expect(response).toEqual(createdWithdrawal);
+      });
+
+      const request = httpTestingController.expectOne({
+        method: 'POST',
+        url:`${environment.apiUrl}/api/transactions/submit/withdrawal/`,
+      });
+
+      request.flush(createdWithdrawal);
+
+      flush();
+
+  }));
+
   // this response is important because the id value in the response will be used
   // to update state by indicating id of the entity to be removed (ngrx/entity)
-  it('should return a response message from backend after deletion ' 
+  it('should return a response message from backend after deletion of deposit' 
       + 'with a message and the expense id', 
     fakeAsync(() => {
        authServiceSpy.getAuthToken.and.returnValue(authData.token);
@@ -119,6 +194,29 @@ fdescribe('TransactionsService', () => {
       });
 
       request.flush(depositDeletionResponse);
+
+      flush()
+
+   }));
+
+  // this response is important because the id value in the response will be used
+  // to update state by indicating id of the entity to be removed (ngrx/entity)
+  it('should return a response message from backend after deletion of withdrawal' 
+      + 'with a message and the expense id', 
+    fakeAsync(() => {
+       authServiceSpy.getAuthToken.and.returnValue(authData.token);
+
+      service.deleteWithdrawal(2).subscribe(response => {
+        expect(response.id).toEqual(2);
+          expect(response).toEqual(withdrawalDeletionResponse);
+      });
+
+      const request = httpTestingController.expectOne({
+        method: 'DELETE',
+        url:`${environment.apiUrl}/api/transactions/submit/withdrawal/2/`,
+      });
+
+      request.flush(withdrawalDeletionResponse);
 
       flush()
 
--- angular/frontend/src/app/test-data/authenticated-user-module-tests/finance-module-tests/savings-accounts-tests/withdrawals-data.ts
+++ angular/frontend/src/app/test-data/authenticated-user-module-tests/finance-module-tests/savings-accounts-tests/withdrawals-data.ts
@@ -1,55 +1,51 @@
-import { incomeSourcesData } from "../../income-sources-tests/income-sources-data";
 import { 
     generateDateOneMonthFromToday,
     generateListOfThreeTestDates 
 } from "src/app/shared-utils/date-helpers.util";
 import { savingsAccountsData } from "./savings-accounts-data";
 import { 
-    TransactionModel, DepositCreateModel, TransactionDeletionResponse 
+    TransactionModel, TransactionDeletionResponse, WithdrawalCreateModel 
 } from "src/app/models/transaction-model";
 
 export const threeDatesThisMonth: [string, string, string] = generateListOfThreeTestDates();
 export const dateOneMonthFromToday: string = generateDateOneMonthFromToday();
 
-export const createdDeposit: TransactionModel = {
-    id: 3, transaction: "Deposit", 
+export const createdWithdrawal: TransactionModel = {
+    id: 3, transaction: "Withdrawal", 
     amount: '300.00',
     date: threeDatesThisMonth[2],
-    income_source: incomeSourcesData[1].id,
     savings_account: savingsAccountsData[1].id,
 };
 
-export const newDepositData: DepositCreateModel = {
+export const newWithdrawalData: WithdrawalCreateModel = {
     amount: 300.00,
     date: threeDatesThisMonth[2],
-    income_source: incomeSourcesData[1].id,
     savings_account: savingsAccountsData[1].id,
 };
 
-export const depositDeletionResponse: TransactionDeletionResponse = {
+export const withdrawalDeletionResponse: TransactionDeletionResponse = {
     id: 2,
     amount: 100.00,
     savings_account: savingsAccountsData[0].id,
-    message: "Deposit successfully deleted!"
+    message: "Withdrawal successfully deleted!"
 }
 
-export const depositsData: TransactionModel[] = [
+
+export const withdrawalsData: TransactionModel[] = [
     { 
-        id: 1, transaction: "Deposit", 
+        id: 1, transaction: "Withdrawal", 
         amount: '100.00',
         date: threeDatesThisMonth[0],
-        income_source: incomeSourcesData[0].id,
-        savings_account: savingsAccountsData[0].id,
+        savings_account: 1,
     },
     { 
-        id: 2, transaction: "Deposit", 
+        id: 2, transaction: "Withdrawal", 
         amount: '200.00',
         date: threeDatesThisMonth[1],
-        income_source: incomeSourcesData[1].id,
         savings_account: savingsAccountsData[1].id,
     }
 ];
 
-export const depsositsPostDeletionData: TransactionModel[] = [
-    depositsData[0]
+export const withdrawalsPostDeletionData: TransactionModel[] = [
+    withdrawalsData[0]
 ];
