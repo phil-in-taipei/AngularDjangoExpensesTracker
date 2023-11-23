fc79de4c91a94df2464b97e1b7200d042a3350a0
phil-in-taipei <sweeneyphilip11@gmail.com>
2023-11-23 16:16:52 +0800

    Savings Account ngrx reducer unit tests to changes to account balance state following submission/deletion of deposit and withdrawal objects

--- angular/frontend/src/app/authenticated-user/finance/savings-accounts/savings-accounts.reducers.spec.ts
+++ angular/frontend/src/app/authenticated-user/finance/savings-accounts/savings-accounts.reducers.spec.ts
@@ -8,12 +8,17 @@ import {
     stateAfterAccountDeletedSuccess, stateAfterAccountDeletedFailure,
     stateAfterNewAccountSubmitted, stateAfterNewAccountSubmittedFailure,
     stateAfterAccountRevised, stateAfterAccountRevisedFailure,
-    stateWithLoadedSavingsAccounts, revisedSavingsAccount
+    stateWithLoadedSavingsAccounts, revisedSavingsAccount, 
+    revisedSavingsAccountAfterDeposit,
+    stateAfterAccountRevisedDueToDeposit, stateAfterAccountRevisedDueToDepositDeletion, 
+    stateAfterAccountRevisedDueToWithdrawal
 } from 'src/app/test-data/authenticated-user-module-tests/finance-module-tests/savings-accounts-tests/savings-accounts-state';
 
 import { 
     SavingsAccountAdded, SavingsAccountAddedCancelled, 
     SavingsAccountDeletionCancelled, SavingsAccountDeletionSaved,
+    SavingsAccountDepositSaved, SavingsAccountDepositDeletionSaved,
+    SavingsAccountWithdrawalSaved, SavingsAccountWithdrawalDeletionSaved, 
     SavingsAccountEditCancelled, SavingsAccountEditUpdated, 
     SavingsAccountsCleared, SavingsAccountsLoaded 
 } from './savings-accounts.actions';
@@ -60,6 +65,54 @@ fdescribe('savingsAccountsReducer', () => {
         expect(state).toEqual(stateAfterAccountRevised.accounts);
     });
 
+    it('returns the state with updated savings accounts entity following succesful ' 
+       + 'submission of deposit (of 200.00 -- string) and indicates that ' 
+       + 'the savings account balance has been sucessfully revised', () => {
+        const state = savingsAccountsReducer(stateWithLoadedSavingsAccounts.accounts, 
+            new SavingsAccountDepositSaved(
+                { amount: '200.00', savingsAccount: savingsAccountsData[1]}
+            )
+        );
+        expect(state).toEqual(stateAfterAccountRevisedDueToDeposit.accounts);
+    });
+
+    it('returns the state with original savings accounts entity following succesful ' 
+       + 'deletion of deposit (of 200.00 -- number) and indicates that ' 
+       + 'the savings account balance has been sucessfully revised', () => {
+        const state = savingsAccountsReducer(stateAfterAccountRevisedDueToDeposit.accounts, 
+            new SavingsAccountDepositDeletionSaved(
+                { amount: 200.00, savingsAccount: revisedSavingsAccountAfterDeposit }
+            )
+        );
+        expect(state).toEqual(stateAfterAccountRevisedDueToDepositDeletion.accounts);
+    });
+
+    // this uses the same state as if there had been a deposit
+    // so the account balance is updated from 200 to 0
+    it('returns the state with updated savings accounts entity following succesful ' 
+       + 'submission of withdrawal (of 200.00 -- string) and indicates that ' 
+       + 'the savings account balance has been sucessfully revised', () => {
+        const state = savingsAccountsReducer(stateAfterAccountRevisedDueToDeposit.accounts, 
+            new SavingsAccountWithdrawalSaved(
+                { amount: '200.00', savingsAccount: revisedSavingsAccountAfterDeposit }
+            )
+        );
+        expect(state).toEqual(stateAfterAccountRevisedDueToWithdrawal.accounts);
+    });
+
+    // this uses the same state as if there had been a deposit of 200
+    // prior to making the withdrawal. so the account balance was 0 prior to 
+    // deleting the withdrawal and is now 200 again 
+    it('returns the state with updated savings accounts entity following succesful ' 
+       + 'submission of withdrawal (of 200.00 -- string) and indicates that ' 
+       + 'the savings account balance has been sucessfully revised', () => {
+        const state = savingsAccountsReducer(stateAfterAccountRevisedDueToWithdrawal.accounts, 
+            new SavingsAccountWithdrawalDeletionSaved(
+                { amount: 200.00, savingsAccount: savingsAccountsData[1] }
+            )
+        );
+        expect(state).toEqual(stateAfterAccountRevisedDueToDeposit.accounts);
+    });
 
     it('returns the state with originally loaded savings accounts entity and indicates that ' 
        + 'the revision of the 2nd savings account was not sucessful', () => {
--- angular/frontend/src/app/test-data/authenticated-user-module-tests/finance-module-tests/savings-accounts-tests/savings-accounts-state.ts
+++ angular/frontend/src/app/test-data/authenticated-user-module-tests/finance-module-tests/savings-accounts-tests/savings-accounts-state.ts
@@ -23,9 +23,36 @@ export const revisedSavingsAccount: SavingsAccountModel =  {
   bank: savingsAccountsData[1].bank 
 }
 
+export const revisedSavingsAccountAfterDeposit: SavingsAccountModel =  { 
+  id: 2, account_name: savingsAccountsData[1].account_name, 
+  account_balance: editedSavingsAccountData.account_balance,
+  account_owner: savingsAccountsData[1].account_owner,
+  currency: savingsAccountsData[1].currency,
+  bank: savingsAccountsData[1].bank 
+}
+
+// this is basically the original data with another name for clarity
+export const revisedSavingsAccountAfterWithdrawal: SavingsAccountModel =  { 
+  id: 2, account_name: savingsAccountsData[1].account_name, 
+  account_balance: savingsAccountsData[1].account_balance,
+  account_owner: savingsAccountsData[1].account_owner,
+  currency: savingsAccountsData[1].currency,
+  bank: savingsAccountsData[1].bank 
+}
+
 const entitiesWithAccountRevised: Dictionary<SavingsAccountModel> = { 
   '1': savingsAccountsData[0],
   '2': revisedSavingsAccount
+}
+
+const entitiesWithAccountRevisedFollowingDeposit: Dictionary<SavingsAccountModel> = { 
+  '1': savingsAccountsData[0],
+  '2': revisedSavingsAccountAfterDeposit
+}
+
+const entitiesWithAccountRevisedFollowingWithdrawal: Dictionary<SavingsAccountModel> = { 
+  '1': savingsAccountsData[0],
+  '2': revisedSavingsAccountAfterWithdrawal
 }
 
 const entitiesWithNewAccountAdded: Dictionary<SavingsAccountModel> = { 
@@ -63,6 +90,36 @@ export const stateAfterAccountRevised = {
   accounts: {
     ids: [2, 1],
     entities: entitiesWithAccountRevised,
+    errorMessage: undefined,
+    savingsAccountsLoaded: true,
+    successMessage: revisedAccountSuccessMessage
+  }
+};
+
+export const stateAfterAccountRevisedDueToDeposit = {
+  accounts: {
+    ids: [2, 1],
+    entities: entitiesWithAccountRevisedFollowingDeposit,
+    errorMessage: undefined,
+    savingsAccountsLoaded: true,
+    successMessage: revisedAccountSuccessMessage
+  }
+};
+
+export const stateAfterAccountRevisedDueToDepositDeletion = {
+  accounts: {
+    ids: [2, 1],
+    entities: entities,
+    errorMessage: undefined,
+    savingsAccountsLoaded: true,
+    successMessage: revisedAccountSuccessMessage
+  }
+};
+
+export const stateAfterAccountRevisedDueToWithdrawal = {
+  accounts: {
+    ids: [2, 1],
+    entities: entitiesWithAccountRevisedFollowingWithdrawal,
     errorMessage: undefined,
     savingsAccountsLoaded: true,
     successMessage: revisedAccountSuccessMessage
